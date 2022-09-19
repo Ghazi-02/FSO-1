b4e832297049264979bc7ef10ab79e4957d3184c
Areeb <auhmirza02@gmail.com>
2022-09-19 13:47:40 +0100

    Part 1d sandbox

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,54 +1,78 @@
 
-// const Hello = ({ name, age }) => {
-//   const bornYear = () => new Date().getFullYear() - age
 
+// const Histroy = (props) => {
+// if(props.allClicks.length === 0) {
 //   return (
 //     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
+//       the app is used by pressing buttons
 //     </div>
 //   )
 // }
-// const App = () => {
-//   const name = "Peter";
-//   const age = 10;
+// return (
+//   <div>
+//     button press history : {props.allClicks.join(" ")}
+//   </div>
+// )
 
-//   return (
-//     <>
-//       <h1>Greetings</h1>
-//       <Hello name="Maya" age={26 + 10} />
-//       <Hello name={name} age={age} />
-
-//     </>
-
-//   )
 // }
 
-const Display = ({counter}) => <div>{counter}</div>
+// const Button = ({handleClick, text}) => (
+//   <button onClick = {handleClick}>
+//     {text}
+//   </button>
+// )
 
-const Button = ({onClick, text}) =>  <button onClick={onClick}>{text}</button>
-  
+// import { useState } from 'react'
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
 
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left + 1)
+//   }
 
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+//       <Button handleClick={handleLeftClick} text="left"/>
+//       <Button handleClick={handleRightClick} text= "right"/>
+//       {right}
+//       <p>{allClicks.join(' ')}</p>
+//       <Histroy allClicks = {allClicks}/>
+//     </div>
+//   )
+// }
+// export default App
 import { useState } from 'react'
-
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+const Display = props => <div>{props.value}</div>
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
 
   return (
-    <>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="Plus" />
-      <Button onClick={decreaseByOne} text="Minus" />
-      <Button onClick={setToZero} text='Zero' />
-    </>
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
   )
-
-}
-
+  }
 export default App
