a066b4c3d1794b91a45d4821796adc676d0af8c9
Areeb <auhmirza02@gmail.com>
2022-09-21 12:43:31 +0100

    1.7: unicafe step2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -9,17 +9,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = good + bad + neutral
+  const average = good - bad
   return (
     <div>
-     <h1>Give Feedback</h1>
-      <Button handleClick ={() => setGood(good + 1)}  text = "Good"/>
-      <Button handleClick ={() => setNeutral(neutral + 1)}text = "Neutral"/>
-      <Button handleClick ={() => setBad(bad + 1)} text = "Bad"/>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
       <h1>Statistics</h1>
       <div>Good {good}</div>
       <div>Neutral {neutral}</div>
       <div>Bad {bad}</div>
+      <div>All {all}</div>
+      <div>Average {average / all}</div>
+      <div>Positive {good / all}</div>
     </div>
   )
 }
